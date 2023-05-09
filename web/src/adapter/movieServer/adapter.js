532f16a63c46c640edd35c4e29c76fc03df10113
Marius De Jager <marius.dejager2@standardbank.co.za>
2023-05-09 20:43:40 +0200

    some idea's

---
+++ web/src/adapter/movieServer/adapter.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+
+const adapter = axios.create()
+
+// functions get, post (url, data, params)
+
+// axios({
+//   method: 'post',
+//   url: '/user/12345',
+//   data: {
+//     firstName: 'Fred',
+//     lastName: 'Flintstone'
+//   }
+// });
