84b6872a63fd9a4cd9eea7c3abc9e1a79352db29
Lisa Nolte <lisan@bbd.co.za>
2023-05-16 22:55:00 +0200

    Clean up

--- web/src/modules/home/home.js
+++ web/src/modules/home/home.js
@@ -1,1 +1,14 @@
 //take data -> process the list into html
+fetch(`http://localhost:8080/`, {
+  method: 'GET',
+  headers: {"Content-Type": "application/json"}
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+  var poster = document.getElementById("poster")
+  poster.src = `https://image.tmdb.org/t/p/w500`+ data.posterPath
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
