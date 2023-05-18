b16def953e1b3a621ac806f45db4efca5e0729d7
Lisa Nolte <lisan@bbd.co.za>
2023-05-18 20:19:37 +0200

    working on bookings, want to pull latest

--- web/src/modules/booking/booking.js
+++ web/src/modules/booking/booking.js
@@ -2,6 +2,12 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const movieId = urlParams.get('movieId');
 
+let cinemaMovieId;
+let date;
+let showId;
+let glasses;
+let seatIds;
+
 fetch(`http://localhost:8080/booking/movie/${movieId}`, {
   method: 'GET',
   headers: {"Content-Type": "application/json"}
@@ -16,6 +22,7 @@ fetch(`http://localhost:8080/booking/movie/${movieId}`, {
     const timeListItem = document.createElement('li');
     timeListItem.addEventListener('click', function () {
       selectTime(this, showTime.time);
+
 		});
     timeListItem.innerHTML = showTime.time;
     timePicker.appendChild(timeListItem);
@@ -25,7 +32,7 @@ fetch(`http://localhost:8080/booking/movie/${movieId}`, {
   console.error('Error:', error);
 });
 
-function selectTime(selectedItem, date) {
+function selectDate(selectedItem, date) {
   const dateItems = document.querySelectorAll('.time-picker li');
   dateItems.forEach(function(item) {
     item.classList.remove('selected');
@@ -45,6 +52,23 @@ function selectTime(selectedItem, time) {
   console.log('Selected time:', time);
   // Your code for handling the selected time
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var dateItems = document.querySelectorAll('#dates li');
+
+  dateItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      this.classList.add('active');
+      var siblings = Array.from(this.parentNode.children).filter(function(el) {
+        return el !== this;
+      }, this);
+      siblings.forEach(function(sibling) {
+        sibling.classList.remove('active');
+      });
+      console.log('active');
+    });
+  });
+});
 
 let ticketCounter = 0;
 
@@ -91,3 +115,22 @@ function glassesDecrement() {
 function updateGlassesCounterValue() {
   document.getElementById('glassesCounter').textContent = glassesCounter.toString();
 }
+
+
+function onClickPostBooking() {
+  //construct json with information
+  //send payload
+
+  fetch(`http://localhost:8080/movie/${movieId}/details`, {
+  method: 'POST',
+  headers: {"Content-Type": "application/json"},
+  body: {
+    "cinemaMovieId": cinemaMovieId, 
+    "date": date, 
+    "showId": showId, 
+    "glasses": glassesCounter, 
+    "seatIds": seatIds
+    }
+  });
+}
+//seats: array of int
