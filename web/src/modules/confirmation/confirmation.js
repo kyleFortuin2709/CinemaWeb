eb8c78432fda0e986a60d8cedae4be5029536e3e
Raaga Krishna <raaga@bbd.co.za>
2023-05-22 20:51:53 +0200

    HTML displays all the required information.

--- web/src/modules/confirmation/confirmation.js
+++ web/src/modules/confirmation/confirmation.js
@@ -10,6 +10,22 @@ fetch(`http://localhost:8080/confirmation/${bookingRef}`, {
 	.then(response => response.json())
 	.then(data => {
 		console.log(data);
+		const poster = document.getElementById("poster")
+		poster.src = `https://image.tmdb.org/t/p/w500`+ data.posterPath
+		
+		const backdrop = document.getElementById("backdrop")
+		backdrop.src = `https://image.tmdb.org/t/p/w500`+ data.backdropPath
+            
+		document.getElementById('movieTitle').textContent = data.title;
+		document.getElementById('movieTitle1').textContent = data.title;
+		document.getElementById('bookingSeats').textContent = data.seats;
+		document.getElementById('bookingSeats1').textContent = data.seats;
+		document.getElementById('movieDate').textContent = data.date;
+		document.getElementById('movieDate1').textContent = data.date;
+		document.getElementById('movieTime').textContent = data.time;
+		document.getElementById('movieTime1').textContent = data.time;
+		document.getElementById('cinemaName').textContent = 'hello';
+		document.getElementById('bookingPrice').textContent = data.price;
 
 	})
 	.catch((error) => {
